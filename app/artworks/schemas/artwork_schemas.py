ccfc0d349beeb8a1d5161d1ce71299f9eca756b7
Tamara Pantic <tamarapanticsd98@gmail.com>
2023-10-20 19:06:38 +0200

    artwork schemas

--- app/artworks/schemas/artwork_schemas.py
+++ app/artworks/schemas/artwork_schemas.py
@@ -1,1 +1,20 @@
 """Artwork schemas"""
+from pydantic import BaseModel, UUID4
+
+
+class ArtworkSchema(BaseModel):
+    """Base Schema for Artwork"""
+
+    id: UUID4
+    stock: int
+    title: str
+    description: str
+    dimensions: str
+    original: bool
+    artist: str
+    creation_date: str
+    price: float
+    image_url: str
+    discount: float
+    category_id: str
+    technique_id: str
